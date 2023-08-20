70a1555817bc53ec5d781543a05366989866e6d5
StarLord <37327630+Adishks101@users.noreply.github.com>
2023-08-20 10:37:06 +0530

    Feat:update inventory response status setter

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import InventoryListView, InventoryDetailView, InventoryCreateView, InventoryListViewByFranchise
+from .views import InventoryListView, InventoryDetailView, InventoryCreateView, InventoryListViewByFranchise, \
+    UpdateInventoryQuantityView
 
 urlpatterns = [
     path('', InventoryListView.as_view(), name='inventory-list'),
     path('create/', InventoryCreateView.as_view(), name='inventory-create'),
     path('franchise/<int:franchise>/', InventoryListViewByFranchise.as_view(), name='franchise-wise-inventory'),
     path('<int:pk>/', InventoryDetailView.as_view(), name='inventory-detail'),
+    path('update/<int:pk>/', UpdateInventoryQuantityView.as_view(), name='update-inventory'),
 ]
